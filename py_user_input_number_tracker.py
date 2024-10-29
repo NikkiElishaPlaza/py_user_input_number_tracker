60b34f163c3f6cab10724776efdb3058b2cffb33
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-29 20:22:48 +0800

    using and added def functions

---
+++ py_user_input_number_tracker.py
@@ -0,0 +1,12 @@
+#Create a program that ask user to input a number ranging from 1 to 50. 
+#Ask the user to input again until the user input is invalid. 
+#When the user input is invalid, display how many inputted numbers are in the following range:
+#1 - 10 = ?
+#11- 20 = ?
+#21- 30 = ?
+#31- 40 = ?
+#41- 50 = ?
+
+def get_user_input():
+def continue_user_input():
+def display_user_input():
