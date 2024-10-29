20d451be62586dea7a4af7bc782120e42e62e6fd
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-29 21:48:34 +0800

    code rearranged and fixed

--- py_user_input_number_tracker.py
+++ py_user_input_number_tracker.py
@@ -11,25 +11,24 @@
 def get_user_input():
 
     print("Welcome to User Input Number Tracker!")
-    while True:
-            user_name = input("Kindly enter your name to start: ")
 
-            print("Thank you for inputting you name.\nTracker starting!")
+    user_name = input("Kindly enter your name to start: ")
+
+    print("Thank you for inputting you name.\nTracker starting!")
     
-            while True:    
-                try:
-                    user_number = int(input("Kindly enter a number ranging from 1 to 50 only: "))
+    while True:    
+        try:
+            user_number = int(input("Kindly enter a number ranging from 1 to 50 only: "))
                     
-                    if 0 < user_number <= 50:
-                        input("Your input has been recorded. Would you like to input a number again? (yes/no): ")
-                        
-                except:
-                    user_number > 50
+            if 0 < user_number <= 50:
+                continue_input = input("Your input has been recorded. Would you like to input a number again? (yes/no): ")
+                
+                if continue_input == "no":
                     print("Thank you for using User Input Number Tracker! Here are your inputted numbers: ")
-
-                if user_number == "no":
                     break
-                elif user_number == "yes":
-                    continue
+                elif continue_input != "yes":
+                    print("Invalid answer. Enter either yes or no only.")
+        except:
+            print("Thank you for using User Input Number Tracker! Here are your inputted numbers: ")
 
 get_user_input()
