add2301fdaf9379b75f9f219ff94973a451a98fc
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-30 13:47:22 +0800

    added comments to the code for formality

--- py_user_input_number_tracker.py
+++ py_user_input_number_tracker.py
@@ -9,14 +9,17 @@
 
 #to get user input
 def get_user_input():
-
+    #greeting user
     print("Welcome to User Input Number Tracker!")
 
+    #asking user name for formality
     user_name = input("Kindly enter your name to start: ")
 
+    #start tracker
     print("Thank you for inputting you name.\nTracker starting!")
 
-    #now making this from the previous demo 
+    #now making this from the previous demo
+    #making this a list
     inputted_number = []
 
     while True:    
@@ -25,16 +28,12 @@ def get_user_input():
                         
                 if 0 < user_number <= 50:
                     inputted_number.append(user_number) #store in an array
-                    continue_input = input("Your input has been recorded. Would you like to input a number again? (yes/no): ")
+
+                    #how the user will continue inputting numbers
+                    continue_input = input("Your input has been recorded. \nWould you like to input a number again? (yes/no): ")
                     
                     if continue_input == "no":
-                        print("Thank you for using User Input Number Tracker! Here are your inputted numbers: ")
-                        #printing them one by one
-                        print(f"1 - 10 = {sum(1 for x in inputted_number if 1 <= x <= 10)}")
-                        print(f"11 - 20 = {sum(1 for x in inputted_number if 11 <= x <= 20)}")
-                        print(f"21 - 30 = {sum(1 for x in inputted_number if 21 <= x <= 30)}")
-                        print(f"31 - 40 = {sum(1 for x in inputted_number if 31 <= x <= 40)}")
-                        print(f"41 - 50 = {sum(1 for x in inputted_number if 41 <= x <= 50)}")
+                        print("Thank you for using User Input Number Tracker!\nHere are your inputted numbers: ")
                         break
                     elif continue_input != "yes":
                         print("Invalid answer. Enter either yes or no only.")
@@ -42,6 +41,5 @@ def get_user_input():
                 print("Thank you for using User Input Number Tracker! Here are your inputted numbers: ")
                 break
 
-
-
+#call funtion
 get_user_input()
