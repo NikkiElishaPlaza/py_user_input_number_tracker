b4e3348262ea98c60393377ec8a12bfd24ae9ca4
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-30 13:40:13 +0800

    added array to store the code and added extra lines to the code

--- py_user_input_number_tracker.py
+++ py_user_input_number_tracker.py
@@ -15,20 +15,33 @@ def get_user_input():
     user_name = input("Kindly enter your name to start: ")
 
     print("Thank you for inputting you name.\nTracker starting!")
-    
+
+    #now making this from the previous demo 
+    inputted_number = []
+
     while True:    
-        try:
-            user_number = int(input("Kindly enter a number ranging from 1 to 50 only: "))
+            try:
+                user_number = int(input("Kindly enter a number ranging from 1 to 50 only: "))
+                        
+                if 0 < user_number <= 50:
+                    inputted_number.append(user_number) #store in an array
+                    continue_input = input("Your input has been recorded. Would you like to input a number again? (yes/no): ")
                     
-            if 0 < user_number <= 50:
-                continue_input = input("Your input has been recorded. Would you like to input a number again? (yes/no): ")
-                
-                if continue_input == "no":
-                    print("Thank you for using User Input Number Tracker! Here are your inputted numbers: ")
-                    break
-                elif continue_input != "yes":
-                    print("Invalid answer. Enter either yes or no only.")
-        except:
-            print("Thank you for using User Input Number Tracker! Here are your inputted numbers: ")
+                    if continue_input == "no":
+                        print("Thank you for using User Input Number Tracker! Here are your inputted numbers: ")
+                        #printing them one by one
+                        print(f"1 - 10 = {sum(1 for x in inputted_number if 1 <= x <= 10)}")
+                        print(f"11 - 20 = {sum(1 for x in inputted_number if 11 <= x <= 20)}")
+                        print(f"21 - 30 = {sum(1 for x in inputted_number if 21 <= x <= 30)}")
+                        print(f"31 - 40 = {sum(1 for x in inputted_number if 31 <= x <= 40)}")
+                        print(f"41 - 50 = {sum(1 for x in inputted_number if 41 <= x <= 50)}")
+                        break
+                    elif continue_input != "yes":
+                        print("Invalid answer. Enter either yes or no only.")
+            except:
+                print("Thank you for using User Input Number Tracker! Here are your inputted numbers: ")
+                break
+
+
 
 get_user_input()
